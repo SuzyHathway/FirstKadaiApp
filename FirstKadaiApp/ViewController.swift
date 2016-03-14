a17de2dcfcb2405637c016c4415efbba0e15a7bf
椎葉寛子 <shiiba_ko@hotmail.co.jp>
2016-03-14 12:40:16 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by 椎葉寛子 on 2016/03/14.
+//  Copyright © 2016年 jp.techacademy.hiroko.shiiba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
